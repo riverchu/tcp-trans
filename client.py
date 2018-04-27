6fb040567f6d65c0dc1d637c77f077198426fc0e
indigo <zeroindigo@163.com>
2018-04-27 23:18:17 +0800

    客户端规范

--- client.py
+++ client.py
@@ -9,23 +9,27 @@ import time
 
 def recv_data(files,dir_name,tcp_socket):
     file_data = tcp_socket.recv(1024)
-    new_file = open(os.path.join(dir_name+"新", files),"wb")
+    new_file = open(os.path.join("new_"+dir_name, files),"wb")
 
     new_file.write(file_data)
     new_file.close()
-    print("文件%s下载完成" % files)
+    print("File %s download." % files)
 
 def main():
+    print("Start client...")
+
+    print("Connecting server...")
     # 创建套接字
     tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     # 连接服务器
     tcp_socket.connect(("120.27.238.190", 9992))
+    print("Connect succeed...")
 
     # 向服务器发送要拷贝的文件夹
-    dir_name = input("请输入要拷贝的文件夹")
+    dir_name = input("Input the name of folder:")
     tcp_socket.send(dir_name.encode())
     # 新建文件夹
-    os.mkdir(dir_name+"新")
+    os.mkdir("new_"+dir_name)
     # 接受文件列表,循环打开文件写入
     file_list = tcp_socket.recv(1024)
     a = eval(file_list)
