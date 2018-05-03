88c169f32382cd24027fd6ac674ba323e4859c0b
indigo <zeroindigo@163.com>
2018-05-04 00:14:19 +0800

    增加当前目录的文件传输

--- client.py
+++ client.py
@@ -9,7 +9,10 @@ import time
 
 def recv_data(files,dir_name,tcp_socket):
     file_data = tcp_socket.recv(1024)
-    new_file = open(os.path.join("new_"+dir_name, files),"wb")
+    if dir_name=='.':
+        new_file = open(os.path.join(dir_name, "new"+files),"wb")
+    else:
+        new_file = open(os.path.join("new_"+dir_name, files),"wb")
 
     new_file.write(file_data)
     new_file.close()
@@ -25,11 +28,15 @@ def main():
     tcp_socket.connect(("120.27.238.190", 9992))
     print("Connect succeed...")
 
-    # 向服务器发送要拷贝的文件夹
+    # 向服务器发送要拷贝的文件夹 绝对路径 是文件夹
     dir_name = input("Input the name of folder:")
     tcp_socket.send(dir_name.encode())
     # 新建文件夹
-    os.mkdir("new_"+dir_name)
+    if dir_name.find(' ')==-1:
+        os.mkdir("new_"+dir_name)
+    else:
+        #不建立文件夹 将文件存在当前目录下
+        dir_name = '.'
     # 接受文件列表,循环打开文件写入
     file_list = tcp_socket.recv(1024)
     a = eval(file_list)
